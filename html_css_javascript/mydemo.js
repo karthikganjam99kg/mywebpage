677de0cbcf3f122a69a750d6c09cff003e6891b9
karthik Ganjam <53421655+karthikganjam99kg@users.noreply.github.com>
2020-07-04 22:55:45 +0530

    Add files via upload

---
+++ html_css_javascript/mydemo.js
@@ -0,0 +1,10 @@
+function sum(a,b){
+    return a+b;
+
+}
+
+let d = s;
+console.log(d());
+function s(sum){
+   return sum(2,3);
+}
---
+++ html_css_javascript/prgm1.js
@@ -0,0 +1,3 @@
+function display(){
+setTimeout(function(){alert("click ok below to see the output!!");},1000);
+}
